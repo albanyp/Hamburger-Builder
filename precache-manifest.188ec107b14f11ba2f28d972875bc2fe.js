af6be86af3e9744e8fc7fcdbd45be8b53d95f5b2
Albany <agpv.dev@gmail.com>
2020-11-29 21:40:35 -0500

    Updates

---
+++ precache-manifest.188ec107b14f11ba2f28d972875bc2fe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452d118363a6b62b431524e9adef36e0",
+    "url": "/hamburger-builder/index.html"
+  },
+  {
+    "revision": "a22db64dd25f825f685f",
+    "url": "/hamburger-builder/static/css/3.945a4fcf.chunk.css"
+  },
+  {
+    "revision": "aa9bb74b1a62d797dd70",
+    "url": "/hamburger-builder/static/css/4.a883389a.chunk.css"
+  },
+  {
+    "revision": "e718d93501eb7b0b42bb",
+    "url": "/hamburger-builder/static/css/5.f00202c9.chunk.css"
+  },
+  {
+    "revision": "a24a45a10f4424a7c5cd",
+    "url": "/hamburger-builder/static/css/main.c3cbd40e.chunk.css"
+  },
+  {
+    "revision": "08c582f7a4014416bd8e",
+    "url": "/hamburger-builder/static/js/2.e86b1337.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/hamburger-builder/static/js/2.e86b1337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a22db64dd25f825f685f",
+    "url": "/hamburger-builder/static/js/3.98532454.chunk.js"
+  },
+  {
+    "revision": "aa9bb74b1a62d797dd70",
+    "url": "/hamburger-builder/static/js/4.dea6de41.chunk.js"
+  },
+  {
+    "revision": "e718d93501eb7b0b42bb",
+    "url": "/hamburger-builder/static/js/5.2a5688b2.chunk.js"
+  },
+  {
+    "revision": "a24a45a10f4424a7c5cd",
+    "url": "/hamburger-builder/static/js/main.3f9e882d.chunk.js"
+  },
+  {
+    "revision": "1fd6151aa5771c78b963",
+    "url": "/hamburger-builder/static/js/runtime-main.91ec83ea.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/hamburger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
